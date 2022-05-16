9b15de9d195d72b858386b91f2a7fda5f1cccfbe
Eduardo Guerrero <104908911+EduardoGuerrero4322IS@users.noreply.github.com>
2022-05-15 21:45:01 -0500

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Del 1 al 10 ¿Que tan guapo soy?")
+    let num;
+    if (num >= 0 && num <= 3){
+        console.log("Nada atractivo");
+    
+    }else if(num >= 4 && num <= 5){
+        console.log("Regularmente guapo");
+    
+    }else if(num >= 6 && num <= 8){
+        console.log("Eres super lindo");
+
+    }else if(num >= 9 && num <= 100){
+        console.log("Estas Guapisimooo");
+    }
+    
