11bdcfa7c295f7e9f5a38244dcb4a452bd94c9a5
Eduardo Guerrero <104908911+EduardoGuerrero4322IS@users.noreply.github.com>
2022-05-16 22:17:55 -0500

    Codigo actualizado

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-console.log("Del 1 al 10 ¿Que tan guapo soy?")
-    let num;
+let num, p1;//Declaracion de Variables 
+
+num = prompt("Del 1 al 10 ¿Que tan guapo soy?");
+    
     if (num >= 0 && num <= 3){
-        console.log("Nada atractivo");
+        alert("Nada atractivo");
     
     }else if(num >= 4 && num <= 5){
-        console.log("Regularmente guapo");
+       alert("Regularmente guapo");
     
     }else if(num >= 6 && num <= 8){
-        console.log("Eres super lindo");
+        alert("Eres super lindo");
 
     }else if(num >= 9 && num <= 100){
-        console.log("Estas Guapisimooo");
+        alert("Estas Guapisimooo");
     }
+    
     
